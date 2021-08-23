0f38b73ec1d2a40d77b9dff7d9fc1661ac1bd495
이창우 <cwadven@naver.com>
2021-08-24 01:19:25 +0900

    feat: CORS 수정
    - CORS 수정

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
