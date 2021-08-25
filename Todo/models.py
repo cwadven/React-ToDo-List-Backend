f77afb9000c72eaa9e0b69e195e66c57e2877503
이창우 <cwadven@naver.com>
2021-08-25 22:44:43 +0900

    chore: table column index 설정
    - index 설정

--- Todo/models.py
+++ Todo/models.py
@@ -5,9 +5,9 @@ from django.db import models
 # To_Do
 class ToDo(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    startDate = models.DateTimeField(null=True, blank=True)
-    completedDate = models.DateTimeField(null=True, blank=True)
-    deadLine = models.DateTimeField(null=True, blank=True)
+    startDate = models.DateTimeField(null=True, blank=True, db_index=True)
+    completedDate = models.DateTimeField(null=True, blank=True, db_index=True)
+    deadLine = models.DateTimeField(null=True, blank=True, db_index=True)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
