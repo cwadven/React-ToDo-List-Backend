47765002188d37d256460ecd42a3104a1a6baeed
cwadven <cwadven@naver.com>
2021-09-04 00:08:11 +0000

    import error fixed

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 from django.utils import timezone
 import datetime
-from env_key import MY_SECRET_KEY
+from .env_key import MY_SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
